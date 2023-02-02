afdb89fedb84026f62b1f559467eb32928bbd020
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-02-01 17:05:09 -0800

    files with white space + extension fixed

--- project1/server.c
+++ project1/server.c
@@ -13,6 +13,9 @@
 #define SPACE "\%20"
 #define SPACE_LEN 3
 
+// TODO: binary files with white space
+// curl -o downloadfile localhost:15635/binary%20file
+
 /* 
     Test File Sizes 
         test.html   449
@@ -22,19 +25,19 @@
         test.png    3418725
 */
 
-char* replaceSpace(const char* s){
-    int i, numSpace = 0;
+char* replaceSpace(const char* s, int* numSpaceReplaced){
+    int i = 0;
+    *numSpaceReplaced = 0;
     char* result;
 
     // search for occurances of %20 in the string
     for (i = 0; s[i] != '\0'; i++) { 
         if (strstr(&s[i], SPACE) == &s[i]) { 
-            numSpace++; 
+            (*numSpaceReplaced)++; 
             i += SPACE_LEN - 1; 
         } 
     } 
-    printf("spaces: %d\n", numSpace);
-    result = (char*)malloc(i + numSpace + (SPACE_LEN - 1) + 1);
+    result = (char*)malloc(i + (*numSpaceReplaced) + (SPACE_LEN - 1) + 1);
 
     // replace all occurrances of %20 with " "
     i = 0;
@@ -48,7 +51,7 @@ char* replaceSpace(const char* s){
         }
     }
     result[i] = '\0';
-    printf("result: %s\n\n", result);
+    // printf("result: %s\n\n", result);
     return result;
 }
 
@@ -90,120 +93,126 @@ int main(int argc, char const* argv[])
         exit(err);
     }
     // 4. Accept a connection from a client, typically blocks until a client connects with the server
-    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-        int err = errno;
-        perror("accept() failed");
-        exit(err);
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+            int err = errno;
+            perror("accept() failed");
+            exit(err);
+        }
+
+        // 5. Read client request
+        valread = read(new_socket, buffer, BUFFER_SIZE); // reads from accept() return value, store in buffer
+        if (valread == -1){
+            int err = errno;
+            perror("read() failed");
+            exit(err);
+        }
+
+        // Parse request
+        char requestType[BUFFER_SIZE] = {0};
+        char requestFile[BUFFER_SIZE] = ".";
+        char contentType[BUFFER_SIZE] = {0};
+        char tmp[BUFFER_SIZE] = {0};
+        char *requestFileType;
+        int numSpaceReplaced = 0;
+
+        // get file extension
+        sscanf(buffer, "%s %s", requestType, tmp);
+        strncat(requestFile, tmp, BUFFER_SIZE);
+        requestFileType = strrchr(requestFile, '.') + 1;
+
+        // handle binary files
+        if (strcmp(tmp, requestFileType) == 0){
+            requestFileType = "binary";
+        }
+
+        // handle files with white space
+        strncpy(requestFile, replaceSpace(requestFile, &numSpaceReplaced), BUFFER_SIZE);
+        if (numSpaceReplaced != 0){
+            requestFileType -= (numSpaceReplaced)*SPACE_LEN - 1;
+        }
+
+        printf("=========\n");
+        printf("spaces: %d\n", numSpaceReplaced);
+        printf("requested: %s\n", requestFile);
+        printf("type: %s\n", requestFileType);
+
+        if (strcmp(requestType, "GET") != 0){
+            int err = errno;
+            perror("incorrect request type");
+            exit(err);
+        }
+
+        if (strcmp(requestFileType, "html") == 0){
+            strncpy(contentType, "text/html; charset=utf-8", BUFFER_SIZE);
+        } else if (strcmp(requestFileType, "txt") == 0){
+            strncpy(contentType, "text/plain; charset=utf-8", BUFFER_SIZE);
+        } else if (strcmp(requestFileType, "pdf") == 0){
+            strncpy(contentType, "application/pdf", BUFFER_SIZE);
+        } else if (strcmp(requestFileType, "jpg") == 0){
+            strncpy(contentType, "image/jpg", BUFFER_SIZE);
+        } else if (strcmp(requestFileType, "png") == 0){
+            strncpy(contentType, "image/png", BUFFER_SIZE);
+        } else if (strcmp(requestFileType, "binary") == 0){
+            strncpy(contentType, "application/octet-stream", BUFFER_SIZE);
+        }
+
+        // Open requested file
+        FILE *fptr;
+        unsigned char *dataBuffer;
+        long filelen;
+
+        fptr = fopen(requestFile, "rb"); // OPEN fptr
+        if (fptr == NULL) { 
+            int err = errno;
+            perror("fopen failed");
+            exit(err);
+        }
+
+        // Get size of file
+        fseek(fptr, 0L, SEEK_END);
+        filelen = ftell(fptr);
+        rewind(fptr);
+
+        // Read the file
+        dataBuffer = (unsigned char *)malloc((filelen) * sizeof(char));
+        int bytes_read = fread(dataBuffer, 1, filelen, fptr);
+        dataBuffer[filelen] = '\0';
+
+        fclose(fptr); // CLOSE fptr
+
+        // 6. Generate and send HTTP response
+        time_t t; 
+        time(&t);
+        char date[30];
+        strftime(date, 30, "%a, %d %b %Y %T %Z", gmtime(&t));
+
+        size_t response_len = snprintf(NULL, 0, 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: %s\r\n"
+            "Content-Length: %ld\r\n"
+            "Date: %s\r\n"
+            // "Connection: close\r\n" // i added this but it does nothing i believe
+            "\r\n", contentType, filelen + 1, date); // need \r\n at the end
+        
+        char* response = (char *)malloc(response_len + filelen); // + 1 or 2?
+        sprintf(response, 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: %s\r\n"
+            "Content-Length: %ld\r\n"
+            "Date: %s\r\n"
+            // "Connection: close\r\n"
+            "\r\n", contentType, filelen, date);
+        memcpy(response + response_len, dataBuffer, filelen);
+        if (send(new_socket, response, response_len + filelen + 1, 0) == -1) { // + 1 or 2?
+            int err = errno;
+            perror("send() failed");
+            exit(err);
+        }
+
+        printf("%s\n", response);    
+        free(dataBuffer); // Free the memory
     }
-
-    // 5. Read client request
-    valread = read(new_socket, buffer, BUFFER_SIZE); // reads from accept() return value, store in buffer
-    if (valread == -1){
-        int err = errno;
-        perror("read() failed");
-        exit(err);
-    }
-
-    // Parse request
-    char requestType[BUFFER_SIZE] = {0};
-    char requestFile[BUFFER_SIZE] = ".";
-    char contentType[BUFFER_SIZE] = {0};
-    char tmp[BUFFER_SIZE] = {0};
-    char *requestFileType;
-    int spaceReplaced = 0;
-
-    // get file extension
-    sscanf(buffer, "%s %s", requestType, tmp);
-    strncat(requestFile, tmp, BUFFER_SIZE);
-    requestFileType = strrchr(requestFile, '.') + 1;
-
-    // handle binary files
-    if (strcmp(tmp, requestFileType) == 0){
-        requestFileType = "binary";
-    }
-
-    // handle files with white space
-    // TODO: fix bug with type offset due to replacing spaces
-    strncpy(requestFile, replaceSpace(requestFile), BUFFER_SIZE);
-
-    printf("requested: %s\n", requestFile);
-    printf("type: %s\n", requestFileType);
-
-    if (strcmp(requestType, "GET") != 0){
-        int err = errno;
-        perror("incorrect request type");
-        exit(err);
-    }
-
-    if (strcmp(requestFileType, "html") == 0){
-        strncpy(contentType, "text/html; charset=utf-8", BUFFER_SIZE);
-    } else if (strcmp(requestFileType, "txt") == 0){
-        strncpy(contentType, "text/plain; charset=utf-8", BUFFER_SIZE);
-    } else if (strcmp(requestFileType, "pdf") == 0){
-        strncpy(contentType, "application/pdf", BUFFER_SIZE);
-    } else if (strcmp(requestFileType, "jpg") == 0){
-        strncpy(contentType, "image/jpg", BUFFER_SIZE);
-    } else if (strcmp(requestFileType, "png") == 0){
-        strncpy(contentType, "image/png", BUFFER_SIZE);
-    } else if (strcmp(requestFileType, "binary") == 0){
-        strncpy(contentType, "application/octet-stream", BUFFER_SIZE);
-    }
-
-    // Open requested file
-    FILE *fptr;
-    unsigned char *dataBuffer;
-    long filelen;
-
-    fptr = fopen(requestFile, "rb"); // OPEN fptr
-    if (fptr == NULL) { 
-        int err = errno;
-        perror("fopen failed");
-        exit(err);
-    }
-
-    // Get size of file
-    fseek(fptr, 0L, SEEK_END);
-    filelen = ftell(fptr);
-    rewind(fptr);
-
-    // Read the file
-    dataBuffer = (unsigned char *)malloc((filelen) * sizeof(char));
-    int bytes_read = fread(dataBuffer, 1, filelen, fptr);
-    dataBuffer[filelen] = '\0';
-
-    fclose(fptr); // CLOSE fptr
-
-    // 6. Generate and send HTTP response
-    time_t t; 
-    time(&t);
-    char date[30];
-    strftime(date, 30, "%a, %d %b %Y %T %Z", gmtime(&t));
-
-    size_t response_len = snprintf(NULL, 0, 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: %s\r\n"
-        "Content-Length: %ld\r\n"
-        "Date: %s\r\n"
-        // "Connection: close\r\n" // i added this but it does nothing i believe
-        "\r\n", contentType, filelen + 1, date); // need \r\n at the end
-    
-    char* response = (char *)malloc(response_len + filelen); // + 1 or 2?
-    sprintf(response, 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: %s\r\n"
-        "Content-Length: %ld\r\n"
-        "Date: %s\r\n"
-        // "Connection: close\r\n"
-        "\r\n", contentType, filelen, date);
-    memcpy(response + response_len, dataBuffer, filelen);
-    if (send(new_socket, response, response_len + filelen + 1, 0) == -1) { // + 1 or 2?
-        int err = errno;
-        perror("send() failed");
-        exit(err);
-    }
-
-    printf("%s\n", response);    
-    free(dataBuffer); // Free the memory
     close(new_socket); // closing the connected socket
     shutdown(server_fd, SHUT_RDWR); // closing the listening socket
     return 0;
